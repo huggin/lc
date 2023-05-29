15e73bea26a336b6a44724aa071a7b506aac7733
huggin <rwangth@gmail.com>
2023-05-28 17:54:29 -0700

    1603. Design Parking System

---
+++ 202305/1603.py
@@ -0,0 +1,9 @@
+class ParkingSystem:
+    def __init__(self, big: int, medium: int, small: int):
+        self.cap = [big, medium, small]
+
+    def addCar(self, carType: int) -> bool:
+        if self.cap[carType - 1] > 0:
+            self.cap[carType - 1] -= 1
+            return True
+        return False
