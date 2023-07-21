b724e637175f584b0d31cbe17a6ea955d3470e90
huggin <1171705+huggin@users.noreply.github.com>
2023-07-20 18:00:12 -0700

    673. Number of Longest Increasing Subsequence

---
+++ 202307/673.py
@@ -0,0 +1,16 @@
+class Solution:
+    def findNumberOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        dp = [1] * n
+        cnt = [1] * n
+        for i in range(n):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        cnt[i] = cnt[j]
+                    elif dp[j] + 1 == dp[i]:
+                        cnt[i] += cnt[j]
+
+        ma = max(dp)
+        return sum(cnt[i] for i in range(n) if dp[i] == ma)
