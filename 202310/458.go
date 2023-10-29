1241e7b0df8a60901cbbb8439b0eb4489b74d39a
huggin <rwangth@gmail.com>
2023-10-28 22:42:55 -0700

    458. Poor Pigs

---
+++ 202310/458.go
@@ -0,0 +1,5 @@
+import "math"
+
+func poorPigs(buckets int, minutesToDie int, minutesToTest int) int {
+	return int(math.Ceil(math.Log2(float64(buckets)) / math.Log2(float64(minutesToTest/minutesToDie)+1)))
+}
