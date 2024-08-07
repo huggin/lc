b5cf1f6e02ed6dfad050ed13f87f9bae2c844dbb
huggin <1171705+huggin@users.noreply.github.com>
2024-08-06 17:14:26 -0700

    273. Integer to English Words

---
+++ 202408/273.py
@@ -0,0 +1,52 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        if num == 0:
+            return "Zero"
+
+        m = {
+            1: "One",
+            2: "Two",
+            3: "Three",
+            4: "Four",
+            5: "Five",
+            6: "Six",
+            7: "Seven",
+            8: "Eight",
+            9: "Nine",
+            10: "Ten",
+            11: "Eleven",
+            12: "Twelve",
+            13: "Thirteen",
+            14: "Fourteen",
+            15: "Fifteen",
+            16: "Sixteen",
+            17: "Seventeen",
+            18: "Eighteen",
+            19: "Nineteen",
+            20: "Twenty",
+            30: "Thirty",
+            40: "Forty",
+            50: "Fifty",
+            60: "Sixty",
+            70: "Seventy",
+            80: "Eighty",
+            90: "Ninety",
+        }
+
+        def helper(num):
+            if num == 0:
+                return ""
+            elif num < 20:
+                return m[num] + " "
+            elif num < 100:
+                return m[num // 10 * 10] + " " + helper(num % 10)
+            elif num < 1000:
+                return m[num // 100] + " Hundred " + helper(num % 100)
+            elif num < 1000000:
+                return helper(num // 1000) + "Thousand " + helper(num % 1000)
+            elif num < 1000000000:
+                return helper(num // 1000000) + "Million " + helper(num % 1000000)
+            else:
+                return helper(num // 1000000000) + "Billion " + helper(num % 1000000000)
+
+        return helper(num).strip()
