d0594a238402f1650a0e63dd27e6dd35326dafb9
huggin <1171705+huggin@users.noreply.github.com>
2024-08-26 17:10:58 -0700

    1514. Path with Maximum Probability

---
+++ 202408/1514.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProbability(
+        self,
+        n: int,
+        edges: List[List[int]],
+        succProb: List[float],
+        start: int,
+        end: int,
+    ) -> float:
+        adj = [[] for _ in range(n)]
+        m = len(edges)
+        for i in range(m):
+            v, w = edges[i][0], edges[i][1]
+            r = succProb[i]
+            adj[v].append((w, r))
+            adj[w].append((v, r))
+
+        dist = [0] * n
+        dist[start] = 1
+        marked = [0] * n
+        pq = []
+        heapq.heappush(pq, (-1, start))
+        while pq:
+            curr, v = heapq.heappop(pq)
+            if marked[v] == 1:
+                continue
+            dist[v] = max(-curr, dist[v])
+            marked[v] = 1
+            for w, r in adj[v]:
+                if marked[w] == 0:
+                    heapq.heappush(pq, (curr * r, w))
+
+        return dist[end]
