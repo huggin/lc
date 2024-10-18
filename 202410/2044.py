e753dec5cb1f01f3c12f2f89aa45bd319c017111
huggin <1171705+huggin@users.noreply.github.com>
2024-10-18 02:13:30 +0000

    2044. Count Number of Maximum Bitwise-OR Subsets

---
+++ 202410/2044.py
@@ -0,0 +1,17 @@
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        a = reduce(lambda x, y : x | y, nums)
+        n = len(nums)
+        self.ans = 0
+
+        def dfs(k, curr):
+            if n == k:
+                if curr == a:
+                    self.ans += 1
+                return
+            dfs(k+1, curr)
+            dfs(k+1, curr | nums[k])
+
+        dfs(0, 0)
+
+        return self.ans
