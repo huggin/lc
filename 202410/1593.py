43907abec4ced372770c613099308140ce596c0f
huggin <1171705+huggin@users.noreply.github.com>
2024-10-21 00:38:16 +0000

    1593. Split a String Into the Max Number of Unique Substrings

---
+++ 202410/1593.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maxUniqueSplit(self, s: str) -> int:
+        a = set()
+        n = len(s)
+
+        self.ans = 0
+        def f(k):
+            if k == n:
+                self.ans = max(self.ans, len(a))
+                return
+
+            for i in range(k, n):
+                if s[k:i+1] not in a:
+                    a.add(s[k:i+1])
+                    f(i+1)
+                    a.remove(s[k:i+1])
+        
+        f(0)
+        return self.ans
