e1df70d0d43a97b6037e34ef91e9273e10836502
huggin <1171705+huggin@users.noreply.github.com>
2024-10-30 00:58:53 +0000

    1671. Minimum Number of Removals to Make Mountain Array

---
+++ 202410/1671.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minimumMountainRemovals(self, nums: List[int]) -> int:
+        n = len(nums)
+        left = [1] * n
+        right = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    left[i] = max(left[i], left[j] + 1)
+
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1, n):
+                if nums[i] > nums[j]:
+                    right[i] = max(right[i], right[j] + 1)
+
+        ans = 0
+        for i in range(n):
+            if left[i] > 1 and right[i] > 1:
+                ans = max(ans, left[i] + right[i] - 1)
+        return n - ans
