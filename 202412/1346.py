f7ff1bce465228d9b6b9017d8dfbd80eecdf9fe3
huggin <1171705+huggin@users.noreply.github.com>
2024-12-01 05:39:59 +0000

    1346. Check If N and Its Double Exist

---
+++ 202412/1346.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        s = set()
+        for a in arr:
+            if a * 2 in s or a % 2 == 0 and a // 2 in s:
+                return True
+            s.add(a)
+        return False
