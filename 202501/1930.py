179c65c06e088e7c745c17e864256667e8e77fa5
huggin <1171705+huggin@users.noreply.github.com>
2025-01-03 16:23:01 -0800

    1930. Unique Length-3 Palindromic Subsequences

---
+++ 202501/1930.py
@@ -0,0 +1,12 @@
+class Solution:
+    def countPalindromicSubsequence(self, s: str) -> int:
+        cnt = [0] * 26
+        v = set()
+        ans = [0] * 26
+        for c in s:
+            if c in v:
+                ans[ord(c) - ord("a")] = cnt[ord(c) - ord("a")].bit_count()
+            for u in v:
+                cnt[ord(u) - ord("a")] |= 1 << ord(c) - ord("a")
+            v.add(c)
+        return sum(ans)
