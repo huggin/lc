27bd1d1d25f845930e9d9663db271a3fe24a4bb6
huggin <user.email=1171705+huggin@users.noreply.github.com>
2025-08-15 00:20:46 +0000

    342. Power of Four

---
+++ 202508/342.py
@@ -0,0 +1,11 @@
+class Solution:
+    def isPowerOfFour(self, n: int) -> bool:
+        if n < 0:
+            return False
+        while n:
+            if n == 1:
+                return True
+            if n % 4 != 0:
+                return False
+            n >>= 2
+        return False
