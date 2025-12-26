8e686280d039ece1905b3d15d13f7f357c43830d
huggin <1171705+huggin@users.noreply.github.com>
2025-12-25 22:22:05 -0800

    2483. Minimum Penalty for a Shop

---
+++ 202512/2483.py
@@ -0,0 +1,15 @@
+class Solution:
+    def bestClosingTime(self, customers: str) -> int:
+        curr = customers.count('Y')
+        n = len(customers)
+        ans = 0
+        ansv = curr
+        for i in range(1, n+1):
+            if customers[i-1] == 'Y':
+                curr -= 1
+            else:
+                curr += 1
+            if ansv > curr:
+                curr = ansv
+                ans = i
+        return ans
