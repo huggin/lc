299634e8afac80a16a59fa30b8c4ce0de08339b2
huggin <1171705+huggin@users.noreply.github.com>
2025-12-28 00:25:53 -0800

    2273. Find Resultant Array After Removing Anagrams

---
+++ 202510/2273.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+        ans = []
+        prev = [0] * 26
+        for w in words:
+            curr = [0] * 26
+            for c in w:
+                curr[ord(c) - ord('a')] += 1
+            if prev != curr:
+                ans.append(w)
+                prev = curr
+        return ans
