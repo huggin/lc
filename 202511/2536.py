e586ae1e791d2d4d4b5a62c5246c13622755f2fb
huggin <1171705+huggin@users.noreply.github.com>
2025-12-28 16:21:07 -0800

    2536. Increment Submatrices by One

---
+++ 202511/2536.py
@@ -0,0 +1,12 @@
+class Solution:
+    def rangeAddQueries(self, n: int, queries: List[List[int]]) -> List[List[int]]:
+        mat = [[0] * n for _ in range(n)]
+        for r1, c1, r2, c2 in queries:
+            for i in range(r1, r2 + 1):
+                mat[i][c1] += 1
+                if c2 + 1 < n:
+                    mat[i][c2 + 1] -= 1
+        for i in range(n):
+            for j in range(1, n):
+                mat[i][j] += mat[i][j - 1]
+        return mat
