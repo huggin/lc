a2644733cae1a5c8aca4e9379e953bc99d51e3b0
huggin <1171705+huggin@users.noreply.github.com>
2026-01-22 16:10:19 -0800

    865. Smallest Subtree with all the Deepest Nodes

---
+++ 202601/865.py
@@ -0,0 +1,42 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def subtreeWithAllDeepest(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        depth = [-1] * 1001
+        def f(p, v):
+            if p is None:
+                return
+            depth[p.val] = v
+            f(p.left, v+1)
+            f(p.right, v+1)
+    
+        f(root, 0)
+        ma = max(depth)
+        cnt = sum(1 for c in depth if c == ma)
+
+        self.ans = None
+        def f2(p):
+            if p is None:
+                return 0
+            left = f2(p.left)
+            right = f2(p.right)
+            if left + right == cnt:
+                if self.ans is None:
+                    self.ans = p
+            
+            if left + right != 0:
+                return left + right
+            if depth[p.val] == ma:
+                if cnt == 1:
+                    if self.ans is None:
+                        self.ans = p
+                return 1
+            return 0
+        
+        f2(root)
+        return self.ans
+            
