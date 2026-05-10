bbbe558b9064eb4ab6627fa28b7643ba0d7f7e64
huggin <1171705+huggin@users.noreply.github.com>
2026-05-09 19:46:44 -0700

    2770. Maximum Number of Jumps to Reach the Last Index

---
+++ 202605/2770.py
@@ -0,0 +1,11 @@
+class Solution:
+    def maximumJumps(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        dp = [-1] * n
+        dp[0] = 0
+        for i in range(1, n):
+            for j in range(i):
+                if dp[j] != -1 and -target <= nums[i] - nums[j] <= target:
+                    dp[i] = max(dp[i], dp[j] + 1)
+
+        return dp[n - 1]
